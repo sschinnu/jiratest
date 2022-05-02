2d2ae53ab50e74e301ff578d4bad9ce9a75c4689
Pavan-Allenki <99476747+Pavan-Allenki@users.noreply.github.com>
2022-05-02 16:12:17 +0530

    Update s3_mfadelete.tf

--- s3_mfadelete.tf
+++ s3_mfadelete.tf
@@ -2,6 +2,7 @@ resource "aws_s3_bucket" "mfadelete" {
   bucket = "examplebuckettftest"
   acl    = "private"
   versioning {
-    mfa_delete = false
+    enabled = true
+    mfa_delete = true
   }
 }
