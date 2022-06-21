094367b11626d34becb4d027773948020559aa3c
iactest <103730049+iactest@users.noreply.github.com>
2022-06-21 10:54:29 +0530

    Update s3_mfadelete.tf

--- s3_mfadelete.tf
+++ s3_mfadelete.tf
@@ -7,4 +7,13 @@ resource "aws_s3_bucket" "mfadelete" {
   }
 }
 
+resource "aws_s3_bucket" "mfadelete2" {
+  bucket = "examplebuckettftest2"
+  acl    = "private"
+  versioning {
+    enabled = true
+    mfa_delete = true
+  }
+}
+
 
