8d14826f532c08f8a498e9b4b74407006dcb19ff
sschinnu <109149371+sschinnu@users.noreply.github.com>
2022-08-17 13:01:22 +0530

    Update file2.tf

--- file2.tf
+++ file2.tf
@@ -6,11 +6,3 @@ resource "aws_s3_bucket" "mfadeletefile" {
     mfa_delete = true
   }
 }
-resource "aws_s3_bucket" "stephy" {
-  bucket = "stephy"
-  acl    = "private"
-  versioning {
-    enabled = true
-    mfa_delete = true
-  }
-}
